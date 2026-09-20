6b51dda8fdb7acee186ec1c30a4a4960c78839d9
emanuelantony983-blip <emanuelantony983@gmail.com>
2026-09-20 16:04:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def register():
         cursor.close()
         conn.close()
         
-        return redirect(url_for('login'))
+       return redirect(url_for('pending_payment'))
         
     return render_template('register.html')
 
